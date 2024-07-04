c5837889afba23614705c99433997aae1d4ac8d2
muhammad-haziqul-khair <mhaziqulkhair93@gmail.com>
2024-07-05 04:06:48 +0500

    Completed GUI of Encode Section

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import customtkinter as ctk
+from tkinter import *
+from tkinter import filedialog
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -17,6 +20,39 @@ class SteganographyApp(ctk.CTk):
         
         self.decode_button = ctk.CTkButton(self.navbar,text="Decode")
         self.decode_button.pack(side="left",fill = "x",expand = True,padx=10,pady=10)
+
+        self.content_frame = ctk.CTkFrame(self)
+        self.content_frame.pack(fill="both", expand=True)
+
+        self.show_encode_window()
+
+    def browse_image(self):
+        self.image_path = filedialog.askopenfilename(title="Browse Image", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
+        self.image_path_variable.set(self.image_path)
+
+
+
+    def show_encode_window(self):
+        for widget in self.content_frame.winfo_children():
+            widget.destroy()
+
+        self.encode_label = ctk.CTkLabel(self.content_frame,text="Encode Message into Image",font=("Arial",18,"bold"))
+        self.encode_label.pack(pady=20)
+
+        self.image_path_variable = StringVar()
+        self.image_path_entry = ctk.CTkEntry(self.content_frame, width=300, placeholder_text="Enter the path of the image",textvariable=self.image_path_variable)
+        self.image_path_entry.pack(pady=10)
+
+        self.select_image_button = ctk.CTkButton(self.content_frame, text="Browse Image",command=self.browse_image)
+        self.select_image_button.pack(pady=10)
+
+
+        self.message_entry = ctk.CTkEntry(self.content_frame, width=300, placeholder_text="Enter Message to Encode")
+        self.message_entry.pack(pady=10)
+
+        self.encode_button = ctk.CTkButton(self.content_frame, text="Encode")
+        self.encode_button.pack(pady=10)
+
 
 
 
