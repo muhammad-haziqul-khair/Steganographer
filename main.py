7ac8ff77ce9bb16b4b58b967297297541f2e87a8
muhammad-haziqul-khair <mhaziqulkhair93@gmail.com>
2024-07-05 04:17:20 +0500

    Making Encode Section Functionable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from tkinter import *
-from tkinter import filedialog
-
+from tkinter import filedialog,messagebox
+from PIL import Image
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -50,11 +50,42 @@ class SteganographyApp(ctk.CTk):
         self.message_entry = ctk.CTkEntry(self.content_frame, width=300, placeholder_text="Enter Message to Encode")
         self.message_entry.pack(pady=10)
 
-        self.encode_button = ctk.CTkButton(self.content_frame, text="Encode")
+        self.encode_button = ctk.CTkButton(self.content_frame, text="Encode",command=self.encode_message)
         self.encode_button.pack(pady=10)
 
 
+    def encode_image(self, image_path, message, output_path):
+        img = Image.open(image_path)
+        encoded_img = img.copy()
+        width, height = img.size
+        message += chr(0)  # Add a null character to mark the end of the message
+        message_bits = ''.join(format(ord(char), '08b') for char in message)
 
+        index = 0
+        for y in range(height):
+            for x in range(width):
+                pixel = list(img.getpixel((x, y)))
+                for n in range(3):  # Loop over RGB values
+                    if index < len(message_bits):
+                        pixel[n] = pixel[n] & ~1 | int(message_bits[index])
+                        index += 1
+                encoded_img.putpixel((x, y), tuple(pixel))
+
+        encoded_img.save(output_path)
+
+
+    def encode_message(self):
+        if hasattr(self,"image_path") and self.image_path:
+            message = self.message_entry.get()
+            if message:
+                output_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG Files", "*.png")])
+                if output_path:
+                    self.encode_image(self.image_path,message,output_path)
+                    messagebox.showinfo("Success", "Message encoded successfully!")
+            else:
+                messagebox.showwarning("Input Error", "Please enter a message to encode.")
+        else:
+            messagebox.showwarning("Input Error", "Please select an image.")
 
 
 if __name__ == "__main__":
